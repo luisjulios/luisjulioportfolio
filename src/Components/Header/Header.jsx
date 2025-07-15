81083db7c063fa3115c07670f068b6af140dc086
Luis Julio <ext_luis.julio@cajalosandes.cl>
2025-07-15 10:01:16 -0400

    [FEAT] Update project configuration and enhance header text

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     <Navbar/>
     <div className='banner'>
       <div className='banner_content'>
-        <h1>Luis Julio - Jr. Developer.</h1>
+        <h1>Luis Julio - Developer.</h1>
         <h3><span>Desarrollador web frontend</span>, autodidacta, creativo, comprometido, con <span>pasión por la programación</span>, y la tecnología. Abierto a <span>afrontar nuevos retos</span>, y con muchas ganas de <span>crecer tanto profesional como personalmente</span>. Sigue bajando para <span>conocer un poco más de mí</span>.</h3>
         <div>
         <small><FaReact/></small>
